69569a9c93e75c89736bc4d828f8a06ef832a08f
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2022-03-12 20:23:15 -0300

    Update relogio.cpp

--- ed-02/relogio.cpp
+++ ed-02/relogio.cpp
@@ -8,6 +8,7 @@ using namespace std;
  void Relogio::formataTempo()  {cout << endl  << setw(2) << setfill('0')<<  _horas  << ":" << setw(2) << _minutos;}
 
 void Relogio::calculaMinutos() {
+ //Nota do Desenvolvedor e Pergunta ao professor, não sabia muito bem onde fazer a validação da hora/minuto, fiquei entre aqui e lá na main, acabei colocando aqui, qual é o ideal?
     if(_horas > HORA_MAX || _minutos > MINUTO_MAX){
         cout << endl <<"Hora ou Minuto invalida.";
     }else{
